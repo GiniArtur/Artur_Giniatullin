ff7022caf2f286e77795d8d4e38db2d12a1d6e12
archi <archigini@gmail.com>
2022-01-28 02:14:14 +0500

    Добавлена стилизация на стриничке аутентификации

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    const borderStyle = OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(36)),
+      borderSide: BorderSide(
+        color: const Color(0xFFbbbbbb), width: 2));
+        const linkTextStyle = TextStyle (fontSize : 19,
+                 fontWeight: FontWeight.bold,
+                 color : Color(0xFF0079D0));
+
     return MaterialApp(
             home: Scaffold(
                 body: Container(
@@ -19,14 +27,17 @@ class MyApp extends StatelessWidget {
                     SizedBox(height: 70,),
                     SizedBox(width: 110, height: 84, child: Placeholder()),
                     SizedBox(height: 20,),
-                    Text('Введите логин в виде 10 цифр номера вашего телефона'),
+                    Text('Введите логин в виде 10 цифр номера вашего телефона',
+                    style: TextStyle (fontSize : 16, color : Color.fromRGBO(0, 0, 0, 0.6)),),
                     SizedBox(height: 20,),
                     const SizedBox(width: 224,
                       child: TextField(
                         decoration: InputDecoration(
                           filled: true ,
                           fillColor: Color(0xFFeceff1),
-                          labelText: '+7'
+                          enabledBorder: borderStyle,
+                          focusedBorder: borderStyle,
+                          labelText: '+7',
                         ),
                       ),
                     ),
@@ -37,6 +48,8 @@ class MyApp extends StatelessWidget {
                         decoration: InputDecoration(
                             filled: true ,
                             fillColor: Color(0xFFeceff1),
+                            enabledBorder: borderStyle,
+                            focusedBorder: borderStyle,
                             labelText: 'Введите пароль'
                         ),
                       ),
@@ -44,11 +57,20 @@ class MyApp extends StatelessWidget {
                     SizedBox(height: 28,),
                     SizedBox (width: 154, height: 42, child: ElevatedButton(
                         onPressed: () {},
-                        child: Text ('Войти'))),
-                    SizedBox(height: 62,),
-                    InkWell(child: Text ('Регистрация'), onTap: () {} ),
+                        child: Text ('Войти'),
+                        style: ElevatedButton.styleFrom(
+                        primary: Color(0xFF0079D0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(36.0)
+                        ),
+                    ),
+                    )),
+                    SizedBox(height: 32,),
+                    InkWell(child: const Text ('Регистрация',
+                      style: linkTextStyle,),
+                        onTap: () {} ),
                     SizedBox(height: 28,),
-                     InkWell (child: Text ("Забыли пар?"), onTap: () {} ),
+                     InkWell (child: Text ("Забыли пароль?", style: linkTextStyle,), onTap: () {} ),
                   ],),
                 ),
             ),
